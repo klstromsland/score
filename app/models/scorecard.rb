ea8e7c33f5d3f0f3804a101f3255f7c90c6cc9dd
trickling <karilynnie@comcast.net>
2016-01-29 17:12:25 -0800

    Heroku config

--- app/models/scorecard.rb
+++ app/models/scorecard.rb
@@ -1,5 +1,3 @@
 class Scorecard < ActiveRecord::Base
   belongs_to :event
-	validates :maxtime_m, presence: true
-	validates :maxtime_s, presence: true
 end
--- app/models/tally.rb
+++ app/models/tally.rb
@@ -1,7 +1,3 @@
 class Tally < ActiveRecord::Base
   belongs_to :event
-	validates :qualifying_score, numericality:
-	{greater_than_or_equal_to: 0}
-	validates :qualifying_scores, numericality:
-	{greater_than_or_equal_to: 0}
 end
