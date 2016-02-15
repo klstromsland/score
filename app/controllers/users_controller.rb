8dafbfa4332b5b0ad0eaa4ae2671956a016ba025
trickling <karilynnie@comcast.net>
2016-02-15 08:23:36 -0800

    heroku-config-f9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,10 +62,16 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+      if @user != current_user
+        if @user.destroy
+          format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      else
+        format.html { redirect_to users_url, notice: 'Unable to remove current user.' }
+        format.json { head :no_content }
+      end
     end
   end
 
